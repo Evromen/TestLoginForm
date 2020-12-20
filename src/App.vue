4fd3715a4eda838a7b2af47acf51015395ae83fb
Dutikov Andrey <dutikov91@mail.ru>
2020-12-21 01:44:17 +0300

    Создана страницы Login.vue и restorePass.vue
    в страницы Login.vue и restorePass.vue добвалены input логина и пароля,кнопки:войти,забыли пароль?,восстановить логин и пароль.
    
    на кнопки:войти,восстановить логин и пароль
    добавлена валидация
    
    Ссылка "забыли пароль?" ,ведет на страницу восстановления пароля(restorePass)
    
    Поля ввода подсвечены(таилвиндом)

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/login">Login</router-link> |
+    <router-link to="/restorePass">Restore password</router-link>
+
+
   </div>
   <router-view/>
 </template>
